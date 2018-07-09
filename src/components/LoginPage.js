01dba719914875d6ee68cd9ed243781cbe031bf1
Tyler P Bellerose <tbellerose@godaddy.com>
2018-07-09 16:52:21 -0700

    Refactor login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,46 +1,51 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { withAuth } from '@okta/okta-react';
+import { checkAuthentication } from '../utils/auth';
 
 export class LoginPage extends Component {
   state = {
-    authenticated: null
+    authenticated: null,
+    userinfo: null
   };
 
-  checkAuthentication = async () => {
-    const authenticated = await this.props.auth.isAuthenticated();
-    if (authenticated !== this.state.authenticated) {
-      this.setState(() => ({
-        authenticated
-      }));
-    }
-  };
+  checkAuthentication = checkAuthentication;
 
   login = async () => {
     this.props.auth.login('/dashboard');
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     this.checkAuthentication();
   };
 
-  componentDidUpdate() {
+  async componentDidUpdate() {
     this.checkAuthentication();
   };
 
   render() {
-    if (this.state.authenticated === null) return null;
-    return this.state.authenticated
-      ? (
-        <Redirect to="/dashboard" />
-      ) : (
-        <div className="box-layout">
-          <div className="box-layout__box">
-            <h1 className="box-layout__title">Agent Toolkit</h1>
-            <button className="button" onClick={this.login}>Login</button>
+    return (
+      <div>
+        {
+          this.state.authenticated !== null &&
+          <div>
+            {
+              this.state.authenticated
+                ? (
+                  <Redirect to="/dashboard" />
+                ) : (
+                  <div className="box-layout">
+                    <div className="box-layout__box">
+                      <h1 className="box-layout__title">Agent Toolkit</h1>
+                      <button className="button" onClick={this.login}>Login</button>
+                    </div>
+                  </div>
+                )
+            }
           </div>
-        </div>
-      );
+        }
+      </div>
+    );
   };
 };
 
