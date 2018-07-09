f7d0074334f9a21a362f6af44c2001f7c8ed64b9
Tyler P Bellerose <tbellerose@godaddy.com>
2018-07-08 22:45:13 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// Import third party libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import styles
+import 'normalize.css/normalize.css';
+
+const jsx = (
+  <div>
+    Hello World
+  </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
