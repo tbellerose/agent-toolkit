a4995afe72b6578c1d8e36c091a30d7b44110f8e
Tyler P Bellerose <tbellerose@godaddy.com>
2018-07-10 16:10:29 -0700

    Add and handle set sites and clear sites actions

---
+++ src/actions/sites.js
@@ -0,0 +1,9 @@
+// SET_SITES
+export const setSites = (sites) => ({
+  type: 'SET_SITES',
+  sites
+});
+
+export const clearSites = () => ({
+  type: 'CLEAR_SITES'
+});
---
+++ src/tests/reducers/sites.test.js
@@ -0,0 +1,25 @@
+import sitesReducer from '../../reducers/sites';
+import sites from '../fixtures/sites';
+
+test('should set default sites state', () => {
+  const state = sitesReducer(undefined, { type: '@@INIT' });
+  expect(state).toEqual([]);
+});
+
+test('should set sites', () => {
+  const action = {
+    type: 'SET_SITES',
+    sites
+  };
+  const state = sitesReducer(undefined, action);
+  expect(state).toEqual(action.sites);
+});
+
+test('should clear sites', () => {
+  let state = sites;
+  const action = {
+    type: 'CLEAR_SITES'
+  };
+  state = sitesReducer(undefined, action);
+  expect(state).toEqual([]);
+});
