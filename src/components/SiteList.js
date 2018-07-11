bba65b62a51add9d97e15e7766eebded128f3a17
Tyler P Bellerose <tbellerose@godaddy.com>
2018-07-10 18:12:38 -0700

    Reformat code

--- src/components/SiteList.js
+++ src/components/SiteList.js
@@ -4,12 +4,13 @@ import SiteListItem from './SiteListItem';
 
 export const SiteList = (props) => (
   <div className="site-list">
-    {props.sites.length > 0 ? (
-      props.sites.map((site) => (
-        <SiteListItem key={site.id} {...site} />
-      ))
-    ) : (
-        <p className="list-item--center">No Sites</p>
+    {props.sites.length > 0
+      ? (
+        props.sites.map((site) => (
+          <SiteListItem key={site.id} {...site} />
+        ))
+      ) : (
+        <div className="list-item--center">No Sites</div>
       )
     }
   </div>
