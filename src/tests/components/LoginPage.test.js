b5ef7e5c0dec95a1224304d6b63f1d80a7ed4fa9
Tyler P Bellerose <tbellerose@godaddy.com>
2018-07-15 22:54:26 -0700

    Debug tests and increase coverage

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -2,10 +2,27 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { LoginPage } from '../../components/LoginPage';
 
-test('should correctly render LoginPage', () => {
-  let wrapper = shallow(<LoginPage />);
+let wrapper, auth;
+
+beforeEach(() => {
+  auth = {
+    isAuthenticated: jest.fn(),
+    login: jest.fn()
+  };
+  wrapper = shallow(<LoginPage auth={auth} />);
+  wrapper.instance().checkAuthentication = jest.fn();
   wrapper.setState(() => ({
     authenticated: false
   }));
+});
+
+test('should correctly render LoginPage', () => {
   expect(wrapper).toMatchSnapshot();
+  expect(auth.isAuthenticated).toHaveBeenCalled();
+  expect(wrapper.instance().checkAuthentication).toHaveBeenCalled();
+});
+
+test('should call login', () => {
+  wrapper.find('button').at(0).simulate('click');
+  expect(auth.login).toHaveBeenCalled();
 });
