155ea47b5f78e81716b934f4e1c90a5e32697cb4
Tyler P Bellerose <tbellerose@godaddy.com>
2018-07-17 16:58:40 -0700

    Add redeploy pod feature

--- src/utils/api.js
+++ src/utils/api.js
@@ -17,7 +17,7 @@ export const getAPI = async (endpoint, authToken) => {
   }
 };
 
-export const deleteAPI = async (endpoint, authToken, body) => {
+export const deleteAPI = async (endpoint, authToken) => {
   try {
     const response = await fetch(`${config.api_uri}${endpoint}`, {
       method: 'DELETE',
@@ -25,6 +25,26 @@ export const deleteAPI = async (endpoint, authToken, body) => {
         'Authorization': `sso-jwt ${authToken}`,
         'Content-Type': 'application/json'
       }
+    });
+    if (response.status !== 204) {
+      const error = await response.json();
+      throw new Error(error.id);
+    }
+    return response;
+  } catch (e) {
+    return { error: e.message };
+  }
+};
+
+export const postAPI = async (endpoint, authToken, body = {}) => {
+  try {
+    const response = await fetch(`${config.api_uri}${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `sso-jwt ${authToken}`,
+        'Content-Type': 'application/json'
+      },
+      body
     });
     if (response.status !== 204) {
       const error = await response.json();
